9fe66d9d347e68d54563e1da43a57c39961d18aa
Nick Wellman <wellman.nicholas@gmail.com>
2020-09-08 14:30:00 -0400

    add logger factory

--- src/main/java/com/lordgasmic/bff/BffApplication.java
+++ src/main/java/com/lordgasmic/bff/BffApplication.java
@@ -1,5 +1,7 @@
 package com.lordgasmic.bff;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,7 +9,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class BffApplication {
-
+    private static final Logger logger = LoggerFactory.getLogger(BffApplication.class);
+    
     public static void main(final String[] args) {
         SpringApplication.run(BffApplication.class, args);
     }

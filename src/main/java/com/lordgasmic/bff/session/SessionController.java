088e62ba857fdb44fda57f13fd67b259367e8390
Nick Wellman <wellman.nicholas@gmail.com>
2020-10-09 15:38:11 -0400

    play with redis

--- src/main/java/com/lordgasmic/bff/session/SessionController.java
+++ src/main/java/com/lordgasmic/bff/session/SessionController.java
@@ -3,6 +3,7 @@ package com.lordgasmic.bff.session;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class SessionController {
     private SessionManager sessionManager;
 
     @GetMapping("/api/v1/session/{id}")
-    public void getSessionInfo(@RequestParam int id) {
+    public void getSessionInfo(@PathVariable int id) {
         sessionManager.getSessionDetails(id);
     }
 }

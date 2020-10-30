56a9f14c2c736e901bfdeaee7279bad588c98a0e
Nick Wellman <wellman.nicholas@gmail.com>
2020-10-30 14:34:52 -0400

    update rest method

--- src/main/java/com/lordgasmic/bff/login/LoginClient.java
+++ src/main/java/com/lordgasmic/bff/login/LoginClient.java
@@ -3,11 +3,11 @@ package com.lordgasmic.bff.login;
 import com.lordgasmic.bff.login.model.LoginRequest;
 import com.lordgasmic.bff.login.model.LoginResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name="login-client", url = "${login-service.url}")
 public interface LoginClient {
 
-    @PutMapping(value="/api/v1/login", headers = {""})
+    @PostMapping(value="/api/v1/login", headers = {""})
     LoginResponse login(LoginRequest request);
 }

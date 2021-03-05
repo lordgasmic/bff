7aae7f208e9aead3c85536f0a34dd00f1dff9a27
Nick Wellman <wellman.nicholas@gmail.com>
2021-03-04 20:29:24 -0500

    add request interceptor and try to make logging work

--- src/main/java/com/lordgasmic/bff/session/SessionController.java
+++ src/main/java/com/lordgasmic/bff/session/SessionController.java
@@ -4,7 +4,6 @@ import com.lordgasmic.bff.session.model.SessionDetails;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j

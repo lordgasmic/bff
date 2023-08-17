0bee19b233db99dbf0af1fd0ff086acc661cdc88
Nick Wellman <wellman.nicholas@gmail.com>
2023-08-17 13:32:42 -0400

    cors

--- src/main/java/com/lordgasmic/bff/configuration/CorsFilter.java
+++ src/main/java/com/lordgasmic/bff/configuration/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", LordgasmicConstants.LORDGASMIC_AUTH_TOKEN.toLowerCase());
+        response.setHeader("Access-Control-Allow-Headers", LordgasmicConstants.LORDGASMIC_AUTH_TOKEN);
         //        response.setHeader("Access-Control-Allow-Headers",
         //                           "Content-Type, Authorization, Content-Length, X-Requested-With, " + LordgasmicConstants.LORDGASMIC_AUTH_TOKEN + ", " + LordgasmicConstants.LORDGASMIC_AUTH_TOKEN.toLowerCase());
         filterChain.doFilter(servletRequest, servletResponse);

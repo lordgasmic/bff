e199185a306b04c4f11f270bc47991e13659b2bd
Nick Wellman <wellman.nicholas@gmail.com>
2024-01-26 11:04:20 -0500

    debug controller

--- src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsController.java
+++ src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +21,7 @@ public class SlackCommandsController {
     }
 
     @PostMapping(value = "/api/v1/slack-commands/notion-scanner", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public Object notionScanner(@RequestHeader final Map<String, String> headers, @RequestBody SlackRequest request) {
+    public Object notionScanner(@RequestHeader final Map<String, String> headers, SlackRequest request) {
         log.info(request.getText());
         String contentType = headers.get("content-type");
         return client.notionScanner(Map.of("content-type", contentType), request.convertToFormData());

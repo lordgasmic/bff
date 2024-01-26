8ca0e5076a23b083995bea7d1110a8ee47c8cd8b
Nick Wellman <wellman.nicholas@gmail.com>
2024-01-26 09:03:10 -0500

    update to post

--- src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsClient.java
+++ src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsClient.java
@@ -1,11 +1,11 @@
 package com.lordgasmic.bff.slackcommands;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "slack-commands", url = "${slack-commands.url}")
 public interface SlackCommandsClient {
 
-    @GetMapping(value = "/api/v1/slack-commands/notion-scanner", headers = {""})
+    @PostMapping(value = "/api/v1/slack-commands/notion-scanner", headers = {""})
     Object notionScanner();
 }

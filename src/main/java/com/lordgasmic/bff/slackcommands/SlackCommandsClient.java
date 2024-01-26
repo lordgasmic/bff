40519851dd0cc24aec608f72de3cc2b4246193d2
Nick Wellman <wellman.nicholas@gmail.com>
2024-01-26 10:53:10 -0500

    debug controller

--- src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsClient.java
+++ src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsClient.java
@@ -2,6 +2,7 @@ package com.lordgasmic.bff.slackcommands;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -11,5 +12,5 @@ import java.util.Map;
 public interface SlackCommandsClient {
 
     @PostMapping(value = "/api/v1/slack-commands/notion-scanner", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    Object notionScanner(@RequestHeader Map<String, String> headers, Map<String, String> request);
+    Object notionScanner(@RequestHeader Map<String, String> headers, MultiValueMap<String, String> request);
 }
--- src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsController.java
+++ src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsController.java
@@ -4,8 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -22,9 +22,9 @@ public class SlackCommandsController {
     }
 
     @PostMapping(value = "/api/v1/slack-commands/notion-scanner", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public Object notionScanner(@RequestHeader final Map<String, String> headers, @RequestParam Map<String, String> request) {
-        log.info(request.get("text"));
+    public Object notionScanner(@RequestHeader final Map<String, String> headers, @RequestBody SlackRequest request) {
+        log.info(request.getText());
         String contentType = headers.get("content-type");
-        return client.notionScanner(Map.of("content-type", contentType), request);
+        return client.notionScanner(Map.of("content-type", contentType), request.convertToFormData());
     }
 }

324ceda4a408449a2d68294eab4de3b271ebd5fa
Nick Wellman <wellman.nicholas@gmail.com>
2024-01-26 10:48:01 -0500

    debug controller

--- src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsClient.java
+++ src/main/java/com/lordgasmic/bff/slackcommands/SlackCommandsClient.java
@@ -2,7 +2,6 @@ package com.lordgasmic.bff.slackcommands;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -12,5 +11,5 @@ import java.util.Map;
 public interface SlackCommandsClient {
 
     @PostMapping(value = "/api/v1/slack-commands/notion-scanner", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    Object notionScanner(@RequestHeader Map<String, String> headers, MultiValueMap<String, String> request);
+    Object notionScanner(@RequestHeader Map<String, String> headers, Map<String, String> request);
 }

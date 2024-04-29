32dd97c5e843ba6c89ece0cfd7a4a74710ead481
Nick Wellman <wellman.nicholas@gmail.com>
2024-04-29 13:14:07 -0400

    save actions

--- src/main/java/com/lordgasmic/bff/BffApplication.java
+++ src/main/java/com/lordgasmic/bff/BffApplication.java
@@ -16,7 +16,6 @@ public class BffApplication {
     private static final Logger logger = LoggerFactory.getLogger(BffApplication.class);
 
     public static void main(final String[] args) {
-
         SpringApplication.run(BffApplication.class, args);
     }
 

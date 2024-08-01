1b3711dffe7342dcae81ea2e893dc78787c96f27
Nick Wellman <lordgasmic@lordgasmic.com>
2024-07-31 22:24:14 -0400

    add feign context

--- src/main/java/com/lordgasmic/bff/BffApplication.java
+++ src/main/java/com/lordgasmic/bff/BffApplication.java
@@ -2,11 +2,14 @@ package com.lordgasmic.bff;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
+@ImportAutoConfiguration(FeignAutoConfiguration.class)
 @Slf4j
 public class BffApplication {
 

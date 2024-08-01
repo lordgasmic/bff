755ccdbf1fd2860647cf743c3758135dd1fff2c8
Nick Wellman <lordgasmic@lordgasmic.com>
2024-07-31 22:06:24 -0400

    comment out everything

--- src/main/java/com/lordgasmic/bff/configuration/WebMvcConfig.java
+++ src/main/java/com/lordgasmic/bff/configuration/WebMvcConfig.java
@@ -1,20 +1,16 @@
 package com.lordgasmic.bff.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    private final LordgasmicRequestInterceptor interceptor;
-
-    public WebMvcConfig(LordgasmicRequestInterceptor interceptor) {
-        this.interceptor = interceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor);
-    }
-}
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    private final LordgasmicRequestInterceptor interceptor;
+//
+//    public WebMvcConfig(LordgasmicRequestInterceptor interceptor) {
+//        this.interceptor = interceptor;
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(interceptor);
+//    }
+//}

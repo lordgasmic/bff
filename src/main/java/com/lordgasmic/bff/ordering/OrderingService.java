67c4b7001b739bb4720149306b46c213a3d416e9
lordgasmic <lordgasmic@lordgasmic.com>
2025-11-20 09:30:47 -0500

    Update OrderingService.java

--- src/main/java/com/lordgasmic/bff/ordering/OrderingService.java
+++ src/main/java/com/lordgasmic/bff/ordering/OrderingService.java
@@ -1,5 +1,6 @@
 package com.lordgasmic.bff.ordering;
 
+import com.google.gson.Gson;
 import com.lordgasmic.bff.ordering.models.OrderingRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
@@ -17,13 +18,15 @@ public class OrderingService {
     private String routingKey;
 
     private final AmqpTemplate rabbitTemplate;
+    private final Gson gson;
 
-    public OrderingService(AmqpTemplate rabbitTemplate) {
+    public OrderingService(AmqpTemplate rabbitTemplate,  Gson gson) {
         this.rabbitTemplate = rabbitTemplate;
+        this.gson = gson;
     }
 
     public void send(final OrderingRequest request) {
-        rabbitTemplate.convertAndSend(exchange, routingKey, request);
+        rabbitTemplate.convertAndSend(exchange, routingKey, gson.toJson(request));
         log.info("LGC:d6d65ca6-88fe-4c06-9521-cfbf3e10d561 - Sent order");
     }
 }

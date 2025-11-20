f0bd0bdc45721053ef11fbf351229c40d241cdf9
Nick Wellman <wellman.nicholas@gmail.com>
2025-11-19 23:34:37 -0500

    add serializable to request object

--- src/main/java/com/lordgasmic/bff/ordering/models/OrderingRequest.java
+++ src/main/java/com/lordgasmic/bff/ordering/models/OrderingRequest.java
@@ -2,11 +2,12 @@ package com.lordgasmic.bff.ordering.models;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
 @Data
-public class OrderingRequest {
+public class OrderingRequest implements Serializable {
     private String message;
     private PrintType type;
     private Map<String, List<String>> properties;

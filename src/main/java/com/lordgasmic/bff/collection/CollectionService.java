c2c2f6544c1d5d1fefbda7d2504ac95e51f5b182
lordgasmic <lordgasmic@lordgasmic.com>
2025-12-09 10:47:16 -0500

    Update CollectionService.java

--- src/main/java/com/lordgasmic/bff/collection/CollectionService.java
+++ src/main/java/com/lordgasmic/bff/collection/CollectionService.java
@@ -12,32 +12,11 @@ public class CollectionService {
         this.client = client;
     }
 
-
     public Object addGas(final GasRequest request) {
         return client.addGas(request);
     }
 
     public Object getGas(final String vehicle) {
         return client.getGas(vehicle);
-    }
-
-    public Object getAllRecipes() {
-        return client.getAllRecipes();
-    }
-
-    public Object getRecipeById(final int id) {
-        return client.getRecipe(id);
-    }
-
-    public Object getIngredientsByRecipeId(final int recipeId) {
-        return client.getIngredientsForRecipeId(recipeId);
-    }
-
-    public Object getTagByRecipeId(final int recipeId) {
-        return client.getTagsForRecipeId(recipeId);
-    }
-
-    public Object getDirectionsForRecipeId(final int recipeId) {
-        return client.getDirectionsForRecipeId(recipeId);
     }
 }

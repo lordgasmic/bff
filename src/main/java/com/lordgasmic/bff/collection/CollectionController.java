00f17549096bfd6357df068a67058004156c3810
lordgasmic <lordgasmic@lordgasmic.com>
2025-12-09 10:47:02 -0500

    Update CollectionController.java

--- src/main/java/com/lordgasmic/bff/collection/CollectionController.java
+++ src/main/java/com/lordgasmic/bff/collection/CollectionController.java
@@ -3,7 +3,6 @@ package com.lordgasmic.bff.collection;
 import com.lordgasmic.bff.collection.model.GasRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CollectionController {
 
     private final CollectionService service;
-
 
     public CollectionController(final CollectionService service) {
         this.service = service;
@@ -28,30 +26,5 @@ public class CollectionController {
     @GetMapping("/api/v1/gas")
     public Object getGas(@RequestParam final String vehicle) {
         return service.getGas(vehicle);
-    }
-
-    @GetMapping("/api/v1/recipe")
-    public Object getAllRecipes() {
-        return service.getAllRecipes();
-    }
-
-    @GetMapping("/api/v1/recipe/{id}")
-    public Object getRecipe(@PathVariable final int id) {
-        return service.getRecipeById(id);
-    }
-
-    @GetMapping("/api/v1/ingredient/{recipeId}")
-    public Object getIngredientsByRecipeId(@PathVariable final int recipeId) {
-        return service.getIngredientsByRecipeId(recipeId);
-    }
-
-    @GetMapping("/api/v1/direction/{recipeId}")
-    public Object getDirectionsByRecipeId(@PathVariable final int recipeId) {
-        return service.getDirectionsForRecipeId(recipeId);
-    }
-
-    @GetMapping("/api/v1/tag/{recipeId}")
-    public Object getTagsByRecipeId(@PathVariable final int recipeId) {
-        return service.getTagByRecipeId(recipeId);
     }
 }

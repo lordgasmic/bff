bbf73d2912b17aa7645eae16e7a2b1a759c4f2b1
lordgasmic <lordgasmic@lordgasmic.com>
2025-12-09 10:46:28 -0500

    Update CollectionClient.java

--- src/main/java/com/lordgasmic/bff/collection/CollectionClient.java
+++ src/main/java/com/lordgasmic/bff/collection/CollectionClient.java
@@ -3,7 +3,6 @@ package com.lordgasmic.bff.collection;
 import com.lordgasmic.bff.collection.model.GasRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,19 +15,4 @@ public interface CollectionClient {
 
     @GetMapping(value = "/api/v1/gas", headers = {""})
     Object getGas(@RequestParam String vehicle);
-
-    @GetMapping(value = "/api/v1/recipe", headers = {""})
-    Object getAllRecipes();
-
-    @GetMapping(value = "/api/v1/recipe/{id}", headers = {""})
-    Object getRecipe(@PathVariable("id") int id);
-
-    @GetMapping(value = "/api/v1/ingredients/{recipeId}", headers = {""})
-    Object getIngredientsForRecipeId(@PathVariable("recipeId") int recipeId);
-
-    @GetMapping(value = "/api/v1/direction/{recipeId}", headers = {""})
-    Object getDirectionsForRecipeId(@PathVariable("recipeId") int recipeId);
-
-    @GetMapping(value = "/api/v1/tag/{recipeId}", headers = {""})
-    Object getTagsForRecipeId(@PathVariable("recipeId") int recipeId);
 }

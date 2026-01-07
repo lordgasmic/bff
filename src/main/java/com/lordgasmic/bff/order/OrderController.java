479c853c38c61f1b85fbde6326c2d53bf01a6b62
Nick Wellman <wellman.nicholas@gmail.com>
2026-01-06 21:30:27 -0500

    send order to order service

--- src/main/java/com/lordgasmic/bff/order/OrderController.java
+++ src/main/java/com/lordgasmic/bff/order/OrderController.java
@@ -5,10 +5,7 @@ import com.lordgasmic.bff.order.models.OrderRequest;
 import com.lordgasmic.bff.session.SessionManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OrderController {
@@ -21,12 +18,18 @@ public class OrderController {
         this.service = service;
     }
 
-    @PostMapping("/api/v1/ordering")
+    @PostMapping("/api/v1/orders")
     public ResponseEntity<Void> send(@RequestHeader(LordgasmicConstants.LORDGASMIC_AUTH_TOKEN) final String token,
                                      @RequestBody final OrderRequest orderingRequest) {
         final String user = sessionManager.getSessionDetails(token).getUsername();
-//        service.send(orderingRequest);
+
+        service.putOrder(user, orderingRequest);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/api/v1/orders")
+    public ResponseEntity<Object> getOrders() {
+        return ResponseEntity.ok(service.getOrders());
     }
 }
--- src/main/java/com/lordgasmic/bff/order/OrderService.java
+++ src/main/java/com/lordgasmic/bff/order/OrderService.java
@@ -1,11 +1,28 @@
 package com.lordgasmic.bff.order;
 
-import lombok.extern.slf4j.Slf4j;
+import com.lordgasmic.bff.order.models.OrderRequest;
+import com.lordgasmic.bff.order.models.OrderRequestOutbound;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 public class OrderService {
 
+    private final OrderClient orderClient;
 
+    public OrderService(final OrderClient orderClient) {
+        this.orderClient = orderClient;
+    }
+
+    public void putOrder(final String user, final OrderRequest request) {
+        final OrderRequestOutbound outbound = new OrderRequestOutbound();
+        outbound.setUser(user);
+        outbound.setMessage(request.getMessage());
+        outbound.setType(request.getType());
+        outbound.setProperties(request.getProperties());
+        orderClient.putOrder(outbound);
+    }
+
+    public Object getOrders() {
+        return orderClient.getOrders();
+    }
 }

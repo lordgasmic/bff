4260611a752d890b2b6fbb3171c27092df84a591
Nick Wellman <wellman.nicholas@gmail.com>
2026-01-07 00:50:14 -0500

    fix service model

--- src/main/java/com/lordgasmic/bff/order/OrderService.java
+++ src/main/java/com/lordgasmic/bff/order/OrderService.java
@@ -1,8 +1,14 @@
 package com.lordgasmic.bff.order;
 
+import com.lordgasmic.bff.order.models.Item;
+import com.lordgasmic.bff.order.models.OrderExtra;
 import com.lordgasmic.bff.order.models.OrderRequest;
 import com.lordgasmic.bff.order.models.OrderRequestOutbound;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class OrderService {
@@ -18,7 +24,13 @@ public class OrderService {
         outbound.setUser(user);
         outbound.setMessage(request.getMessage());
         outbound.setType(request.getType());
-        outbound.setProperties(request.getProperties());
+
+        final Map<String, List<OrderExtra>> props = new HashMap<>();
+        for (final Item item : request.getProperties()) {
+            props.put(item.getItem(), item.getExtras());
+        }
+        outbound.setProperties(props);
+
         orderClient.putOrder(outbound);
     }
 
